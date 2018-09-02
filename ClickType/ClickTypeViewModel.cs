26a3ee820a313a3e8ef51376847c5a092f597483
Amol Bhagwat <amolrbhagwat@gmail.com>
2018-09-02 19:04:21 -0400

    Load snippets in the ViewModel
    Closes #2

--- ClickType/ClickTypeViewModel.cs
+++ ClickType/ClickTypeViewModel.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClickType
 {
-    class ClickTypeViewModel
+    class ClickTypeViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private List<Snippet> snippets;
+        public List<Snippet> Snippets
+        {
+            private set
+            {
+                snippets = value;
+                RaisePropertyChanged("Snippets");
+            }
+            get
+            {
+                return snippets;
+            }
+        }
+
+        public ClickTypeViewModel()
+        {
+            Snippets = SnippetLoader.LoadSnippets();
+        }
+
+        protected void RaisePropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
