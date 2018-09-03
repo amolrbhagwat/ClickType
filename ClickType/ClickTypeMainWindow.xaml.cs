0b8f9fa96ba2140a138ae12a0bf8afbae804961c
Amol Bhagwat <amolrbhagwat@gmail.com>
2018-09-02 22:30:41 -0400

    Make the app a palette window
    Set WS_EX_NOACTIVATE
    Clear WS_MAXIMIZE
    Set Topmost to True
    Closes #3

--- ClickType/ClickTypeMainWindow.xaml.cs
+++ ClickType/ClickTypeMainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -20,6 +22,21 @@ namespace ClickType
     /// </summary>
     public partial class ClickTypeMainWindow : Window
     {
+        private const int WS_EX_NOACTIVATE = 0x08000000;
+        private const int WS_MAXIMIZEBOX = 0x10000;
+
+        // Window style
+        private const int GWL_STYLE = -16;
+        // Extended Window style
+        private const int GWL_EXSTYLE = -20;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+
+
         public ClickTypeMainWindow()
         {
             InitializeComponent();
@@ -27,6 +44,19 @@ namespace ClickType
 
         private void SnippetsListBoxItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
+        }
+
+        private void Window_SourceInitialized(object sender, EventArgs e)
+        {
+            WindowInteropHelper interopHelper = new WindowInteropHelper(this);
+
+            // Disable the maximize button
+            int style = GetWindowLong(interopHelper.Handle, GWL_STYLE);
+            SetWindowLong(interopHelper.Handle, GWL_STYLE, style & ~WS_MAXIMIZEBOX);
+
+            // Set WS_EX_NOACTIVATE - this will enable interaction with the window without grabbing focus
+            int exStyle = GetWindowLong(interopHelper.Handle, GWL_EXSTYLE);
+            SetWindowLong(interopHelper.Handle, GWL_EXSTYLE, exStyle | WS_EX_NOACTIVATE);
         }
     }
 }
