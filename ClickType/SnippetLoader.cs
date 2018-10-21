f0578f5912c1aecb38425707555278ff78292b83
Amol Bhagwat <amolrbhagwat@gmail.com>
2018-10-20 20:30:44 -0400

    Add code to return all snippets from the db

--- ClickType/SnippetLoader.cs
+++ ClickType/SnippetLoader.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace ClickType
 {
@@ -10,8 +14,20 @@ namespace ClickType
     {
         public static List<Snippet> LoadSnippets()
         {
-            List<Snippet> snippets = new List<Snippet>();
+            List<Snippet> snippets;
+
+            using (IDbConnection dbConnection = new SQLiteConnection(LoadConnectionString()))
+            {
+                var queryResults = dbConnection.Query<Snippet>("select * from Snippet", new DynamicParameters());
+                snippets = queryResults.ToList();
+            }
+
             return snippets;
+        }
+
+        private static string LoadConnectionString(string id="Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
         }
     }
 }
